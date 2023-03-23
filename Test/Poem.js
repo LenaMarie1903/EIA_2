89d6f966a750982ba7f68cd682fb6503bf425ff0
LenaMarie1903 <lena.m.fleig@gmail.com>
2023-03-23 14:27:52 +0100

    Rückgabe zur Schleife

--- Test/Poem.js
+++ Test/Poem.js
@@ -1,7 +1,6 @@
 "use strict";
 var Poem;
 (function (Poem) {
-    debugger;
     let Subjekt = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
     let verb = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
     let Objekt = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
@@ -11,19 +10,21 @@ var Poem;
     for (let i = 6; i >= 1; i--) {
         //console.log(i)
         let sentcence = getVerse(Subjekt, verb, Objekt);
-        //console.log(sentcence)
+        console.log(sentcence);
         //let erstes=[];
         //let zweites=[];
         //let drittens=[];
     }
-    function getVerse(_Subjekt, _verb, _Objekt) {
+    function getVerse(_Subj, _ver, _Obj) {
         let vers = "";
-        let zufallzahl_S = Math.floor(Math.random() * _Subjekt.length);
-        let zufallzahl_v = Math.floor(Math.random() * _verb.length);
-        let zufallzahl_O = Math.floor(Math.random() * _Objekt.length);
-        console.log(zufallzahl_S);
+        let zufallzahl_S = Math.floor(Math.random() * _Subj.length);
+        let zufallzahl_v = Math.floor(Math.random() * _ver.length);
+        let zufallzahl_O = Math.floor(Math.random() * _Obj.length);
+        //console.log(zufallzahl_S);
+        //Der random Wert wird aus Array rausgeschnitten und in Variable vers reingeschrieben als string. Die 1 ist die Anzahl die gesplicet wird. Da ich einem string kein Array anhängen kann, muss ich die Stelle angeben hier O (erste Stelle)
+        vers = _Subj.splice(zufallzahl_S, 1)[0] + " " + _ver.splice(zufallzahl_v, 1)[0] + " " + _Obj.splice(zufallzahl_O, 1)[0];
         //let result: string = _Subjekt[0];
-        //return result;
+        return vers;
     }
 })(Poem || (Poem = {}));
 //# sourceMappingURL=Poem.js.map
