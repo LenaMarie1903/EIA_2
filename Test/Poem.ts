9ff25c97f19035efbc086b1f97f9c8c8d8dc2aab
LenaMarie1903 <lena.m.fleig@gmail.com>
2023-03-23 13:58:51 +0100

    Zufallszahl

--- Test/Poem.ts
+++ Test/Poem.ts
@@ -1,5 +1,5 @@
 namespace Poem {
-
+   debugger;
    let Subjekt: string[] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"]
    let verb: string[] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"]
    let Objekt: string[] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"]
@@ -9,9 +9,9 @@ namespace Poem {
    //brewconsole.log(Objekt.length);
 
    for (let i = 6; i >= 1; i--) {
-      console.log(i)
+      //console.log(i)
       let sentcence: string = getVerse(Subjekt, verb, Objekt);
-      console.log(sentcence)
+      //console.log(sentcence)
       //let erstes=[];
       //let zweites=[];
       //let drittens=[];
@@ -22,10 +22,16 @@ namespace Poem {
 
       let vers : string = "";
 
-      let zufallzahl_S : number = Math.floor(Math.random)*Subjekt.length
+      let zufallzahl_S : number = Math.floor(Math.random()*_Subjekt.length);
+      let zufallzahl_v : number = Math.floor(Math.random()*_verb.length);
+      let zufallzahl_O : number = Math.floor(Math.random()*_Objekt.length);
+
+      console.log(zufallzahl_S);
+     
+      
 
 
-      //let result: string = "Alohomora";
+      //let result: string = _Subjekt[0];
       //return result;
       
      
