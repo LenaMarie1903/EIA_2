89d16f29969279dea15dc18b25164d6783cd1982
LenaMarie1903 <lena.m.fleig@gmail.com>
2023-03-30 14:45:22 +0200

    Aufgabe"_code

--- A2/Eventinspector.js
+++ A2/Eventinspector.js
@@ -1,2 +1,35 @@
 "use strict";
+var Aufgabe2;
+(function (Aufgabe2) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad() {
+        document.addEventListener("mousemove", setInfoBox);
+        document.addEventListener("click", logInfo);
+        document.addEventListener("keyup", logInfo);
+        document.body.addEventListener("click", logInfo);
+        document.body.addEventListener("keyup", logInfo);
+        document.getElementById("#div0")?.addEventListener("click", logInfo);
+        document.getElementById("#div0")?.addEventListener("keyup", logInfo);
+        document.getElementById("#div1")?.addEventListener("click", logInfo);
+        document.getElementById("#div1")?.addEventListener("keyup", logInfo);
+    }
+    //document.body.addEventListener("mousemove", setInfoBox); übergint das dem _eveent 
+    //Frage Woher weiß Zeile 9, dass es ein mousemove Event ist? 
+    function setInfoBox(_event) {
+        //Er weiß nicht,dass span wirklich existiert. Daher muss ich durch eine if Funktion  überprüfeb, ob das HTML Element span in HTML wirklich vorhanden ist.
+        let span = document.getElementById("#span0");
+        let xvalue = _event.clientX;
+        let yvalue = _event.clientY;
+        if (span != null) {
+            span.innerText = "Mouseposition: " + xvalue + "," + yvalue + _event.target;
+            span.setAttribute('style', 'right: xvalue + 10px');
+            span.setAttribute('style', 'top: yvalue + 10px');
+        }
+        //span.setAttribute('style','right: xvalue + 10px');
+        // span.setAttribute('style', 'top: yvalue + 10px');
+    }
+    function logInfo(_event) {
+        console.log(_event.target, _event.currentTarget, _event.type, _event);
+    }
+})(Aufgabe2 || (Aufgabe2 = {}));
 //# sourceMappingURL=Eventinspector.js.map
---
+++ A2/Eventinspector.ts
@@ -0,0 +1,62 @@
+namespace Aufgabe2{
+
+    window.addEventListener("load", handleLoad);
+
+
+function handleLoad () {
+
+
+    document.addEventListener("mousemove", setInfoBox);
+
+
+    document.addEventListener("click",logInfo);
+    document.addEventListener("keyup",logInfo);
+
+    document.body.addEventListener("click",logInfo);
+    document.body.addEventListener("keyup", logInfo);
+
+    document.getElementById("#div0")?.addEventListener("click",logInfo);
+    document.getElementById("#div0")?.addEventListener("keyup",logInfo);
+
+    document.getElementById("#div1")?.addEventListener("click",logInfo);
+    document.getElementById("#div1")?.addEventListener("keyup",logInfo);
+
+}
+//document.body.addEventListener("mousemove", setInfoBox); übergint das dem _eveent 
+//Frage Woher weiß Zeile 9, dass es ein mousemove Event ist? 
+function setInfoBox(_event: MouseEvent) {
+
+ //Er weiß nicht,dass span wirklich existiert. Daher muss ich durch eine if Funktion  überprüfeb, ob das HTML Element span in HTML wirklich vorhanden ist.
+    let span : HTMLElement|null =document.getElementById("#span0");
+    let xvalue: number = _event.clientX;
+    let yvalue: number = _event.clientY;
+    if(span != null){
+        span.innerText= "Mouseposition: " + xvalue + ","  + yvalue + _event.target;
+
+        span.setAttribute('style','right: xvalue + 10px');
+        span.setAttribute('style', 'top: yvalue + 10px');
+    }
+
+    //span.setAttribute('style','right: xvalue + 10px');
+   // span.setAttribute('style', 'top: yvalue + 10px');
+
+
+
+
+}
+
+function logInfo(_event: Event) {
+
+    console.log(_event.target,_event.currentTarget,_event.type, _event);
+
+}
+
+
+
+
+
+
+
+
+
+}
