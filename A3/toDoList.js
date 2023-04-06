a9a9d3aee6789b3f157b5b3636a53a2a5fc315c7
LenaMarie1903 <lena.m.fleig@gmail.com>
2023-04-06 19:00:26 +0200

    TS und CSS

--- A3/toDoList.js
+++ A3/toDoList.js
@@ -1,2 +1,17 @@
 "use strict";
+var ToDo;
+(function (ToDo) {
+    window.addEventListener("load", handleload);
+    function handleload(_event) {
+    }
+    document.querySelector("#finish").addEventListener('click', function () {
+        console.log("Ich bin fertig");
+    });
+    document.querySelector("#edit").addEventListener('click', function () {
+        console.log("Ich bin bearbeitet");
+    });
+    document.querySelector("#trashbin").addEventListener('click', function () {
+        console.log("Ich bin gelöscht");
+    });
+})(ToDo || (ToDo = {}));
 //# sourceMappingURL=toDoList.js.map
--- A3/toDoList.ts
+++ A3/toDoList.ts
@@ -1,5 +1,27 @@
 namespace ToDo {
+    window.addEventListener("load", handleload);
 
+    function handleload(_event: Event): void {
+
+    }
+
+
+    document.querySelector("#finish")!.addEventListener('click', function () {
+
+        console.log("Ich bin fertig");
+
+    })
+
+    document.querySelector("#edit")!.addEventListener('click', function () {
+
+        console.log("Ich bin bearbeitet");
+
+    })
+    document.querySelector("#trashbin")!.addEventListener('click', function () {
+
+        console.log("Ich bin gelöscht");
+
+    })
 
 
 
